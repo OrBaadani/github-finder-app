6fe3908d0eabc01d3990bb6eff57e48ea5833a16
Or Baadani <orbaadani@gmail.com>
2022-02-01 16:29:42 +0200

    changed params for api request

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -23,9 +23,13 @@ export const searchUsers = async (text) => {
 };
 
 export const getUserAndRepos = async (login) => {
+    const params = new URLSearchParams({
+        sort: created,
+        per_page: 10,
+    });
     const [user, repos] = await Promise.all([
         githubAPI.get(`/users/${login}`),
-        githubAPI.get(`/users/${login}/repos`),
+        githubAPI.get(`/users/${login}/repos?${params}`),
     ]);
 
     return { user: user.data, repos: repos.data };
